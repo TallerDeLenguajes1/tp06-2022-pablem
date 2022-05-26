f97eb08b1efb8efa9d21035687fed7677c3d4986
bobadillajuan <juampi348@gmail.com>
2022-05-26 17:56:36 -0300

    metodo salario

--- tp6e2/Empleado.cs
+++ tp6e2/Empleado.cs
@@ -25,5 +25,31 @@ public class Empleado
     {
         return Convert.ToInt32((DateTime.Now - fechaNacimiento).Days/365.2425);
     }
-    
-}
+    // i) 1 % del sueldo básico por cada año de antigüedad hasta los
+    // 20 años, a partir de este, el porcentaje se fija en 25%.
+    // ii) Si el cargo es Ingeniero o Especialista, el Adicional se
+    // incrementa en un 50%.
+    // iii) Si es casado al Adicional se le aumenta $15000.
+    // Por ejemplo, si la antigüedad es de 15 años y el Sueldo Básico =
+    // $65000, el Adicional es 65000 * 0.15 = 9750. Si además el cargo
+    // es Ingeniero o Especialista, se incrementa el Adicional en un
+    // 50%. Esto es: 9750* 1.50 = 14625. Si a su vez es casado el
+    // Adicional será: 14625+ 15000= 29625
+    public double salario() 
+    {
+        double total = sueldo;
+
+        if(antiguedad()>20) {
+            total += 0.25*sueldo;
+        } else {
+            total += 0.01*antiguedad()*sueldo;
+        }
+        if(cargo==Cargos.Ingeniero || cargo==Cargos.Especialista) {
+            total += 0.5*sueldo;//0.5*total error de enunciado?
+        }
+        if(estadoCivil == 'c') {
+            total += 15000;
+        }
+        return total;
+    }
+}  
