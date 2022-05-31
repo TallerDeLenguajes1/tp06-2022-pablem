9c71a4eb9e7d4172a5ca997c61ab5677047e9bd2
pablo <palmierijpablo@gmail.com>
2022-05-31 12:16:13 -0300

    ejercicio 2 listo

--- tp6e2/Program.cs
+++ tp6e2/Program.cs
@@ -12,7 +12,7 @@ emp1.Nombre = "nombre01";
 emp1.Apellido = "apellido01";
 emp1.FechaNacimiento = new DateTime(2001,1,1);
 emp1.FechaIngreso = new DateTime(2020,1,1);
-emp1.EstadoCivil = 'c';
+emp1.EstadoCivil = 's';
 emp1.Genero = 'f';
 emp1.Sueldo = 10000;
 emp1.Cargo = Cargos.Auxiliar;
@@ -20,29 +20,23 @@ emp1.Cargo = Cargos.Auxiliar;
 Empleado emp2 = new Empleado();
 emp2.Nombre = "nombre02";
 emp2.Apellido = "apellido02";
-emp2.FechaNacimiento = new DateTime(1091,1,1);
-emp2.FechaIngreso = new DateTime(2022,1,1);
-emp2.EstadoCivil = 's';
-emp2.Genero = 'm';
+emp2.FechaNacimiento = new DateTime(1992,1,1);
+emp2.FechaIngreso = new DateTime(2020,1,1);
+emp2.EstadoCivil = 'c';
+emp2.Genero = 'f';
 emp2.Sueldo = 10000;
-emp2.Cargo = Cargos.Administrativo;
+emp2.Cargo = Cargos.Ingeniero;
 
+//Empleado 3 se carga por consola
 Empleado emp3 = new Empleado();
 Console.WriteLine("+ Empleado 3. Nombre: ");
 emp3.Nombre = Console.ReadLine();
 Console.WriteLine("Apellido: ");
 emp3.Apellido = Console.ReadLine();
-Console.WriteLine("Fecha de nacimiento: ");
-int d = Convert.ToInt16(Console.ReadLine());
-int m = Convert.ToInt16(Console.ReadLine());
-int a = Convert.ToInt16(Console.ReadLine());
-emp3.FechaNacimiento = new DateTime(a,m,d);
-Console.WriteLine("Fecha de Ingreso: ");
+Console.WriteLine("Fecha de nacimiento: (aaaa-mm-dd)");
+emp3.FechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+Console.WriteLine("Fecha de Ingreso: (aaaa-mm-dd)");
 emp3.FechaIngreso = Convert.ToDateTime(Console.ReadLine());
-// d = Convert.ToInt16(Console.ReadLine());
-// m = Convert.ToInt16(Console.ReadLine());
-// a = Convert.ToInt16(Console.ReadLine());
-// emp3.FechaIngreso = new DateTime(a,m,d);
 Console.WriteLine("Estado civil: ");
 emp3.EstadoCivil = Convert.ToChar(Console.ReadLine());
 Console.WriteLine("Género: ");
@@ -61,7 +55,7 @@ double total = 0;
 foreach (var emp in listaEmpleados) {
     total += emp.salario();
 }
-Console.WriteLine("\nMonto total = ", total);
+Console.WriteLine("\nMonto total = "+total);
 
 Empleado aux1 = listaEmpleados[0];
 int aux2 = listaEmpleados[0].aniosParaJubilarse();
@@ -76,11 +70,11 @@ Console.WriteLine("Fecha de nacimiento: "+aux1.FechaNacimiento.ToString("dd/MM/y
 Console.WriteLine("Estado civil: "+aux1.EstadoCivil);
 Console.WriteLine("Género: "+aux1.Genero);
 Console.WriteLine("Fecha de ingreso: "+aux1.FechaIngreso.ToString("dd/MM/yyyy"));
-Console.WriteLine("Sueldo básico: ", aux1.Sueldo);
-Console.WriteLine("Cargo: ", aux1.Cargo);
+Console.WriteLine("Sueldo básico: "+aux1.Sueldo);
+Console.WriteLine("Cargo: "+aux1.Cargo);
 
-Console.WriteLine("\nAntigüedad: ", aux1.antiguedad());
-Console.WriteLine("Edad: ", aux1.edad());
-Console.WriteLine("Años para jubilarse: ", aux1.aniosParaJubilarse());
-Console.WriteLine("Salario: ", aux1.salario());
+Console.WriteLine("\nAntigüedad: "+aux1.antiguedad()+" años");
+Console.WriteLine("Edad: "+aux1.edad());
+Console.WriteLine("Años para jubilarse: "+aux1.aniosParaJubilarse()+" años");
+Console.WriteLine("Salario: "+aux1.salario());
 
