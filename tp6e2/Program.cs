54712d953be4a6d743c921ddd6247c4e7dc4a653
pablo <palmierijpablo@gmail.com>
2022-05-31 00:50:53 -0300

    da error

--- tp6e2/Program.cs
+++ tp6e2/Program.cs
@@ -1,4 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//IMPLEMENTACIÓN CON LISTA:
+// var emp = new List<Empleado>();
+// Empleado nuevo; 
+// for (int i = 0; i < 3; i++) {
+//     nuevo = new Empleado();
+
+//     emp.Add(nuevo);
+// }
 
 Empleado emp1 = new Empleado();
 emp1.Nombre = "nombre01";
@@ -26,14 +33,15 @@ emp3.Nombre = Console.ReadLine();
 Console.WriteLine("Apellido: ");
 emp3.Apellido = Console.ReadLine();
 Console.WriteLine("Fecha de nacimiento: ");
-// int d = Console.Read();
-// int m = Console.Read();
-// int a = Console.Read();
-// emp3.FechaNacimiento = new DateTime(a,m,d);
-// Console.WriteLine("Fecha de Ingreso: ");
-// d = Console.Read();
-// m = Console.Read();
-// a = Console.Read();
+int d = Convert.ToInt16(Console.ReadLine());
+int m = Convert.ToInt16(Console.ReadLine());
+int a = Convert.ToInt16(Console.ReadLine());
+emp3.FechaNacimiento = new DateTime(a,m,d);
+Console.WriteLine("Fecha de Ingreso: ");
+emp3.FechaIngreso = Convert.ToDateTime(Console.ReadLine());
+// d = Convert.ToInt16(Console.ReadLine());
+// m = Convert.ToInt16(Console.ReadLine());
+// a = Convert.ToInt16(Console.ReadLine());
 // emp3.FechaIngreso = new DateTime(a,m,d);
 Console.WriteLine("Estado civil: ");
 emp3.EstadoCivil = Convert.ToChar(Console.ReadLine());
@@ -44,4 +52,35 @@ emp3.Sueldo = Convert.ToDouble(Console.ReadLine());
 Console.Write("Cargo: ");
 emp3.Cargo = (Cargos) Convert.ToInt16(Console.ReadLine());
 
-Console.WriteLine(emp3.Cargo);
+var listaEmpleados = new List<Empleado>();
+listaEmpleados.Add(emp1);
+listaEmpleados.Add(emp2);
+listaEmpleados.Add(emp3);
+
+double total = 0;
+foreach (var emp in listaEmpleados) {
+    total += emp.salario();
+}
+Console.WriteLine("\nMonto total = ", total);
+
+Empleado aux1 = listaEmpleados[0];
+int aux2 = listaEmpleados[0].aniosParaJubilarse();
+foreach (var emp in listaEmpleados) {
+    if (emp.aniosParaJubilarse()<aux2) {
+        aux1 = emp;
+    }
+    aux2 = emp.aniosParaJubilarse();
+}
+Console.WriteLine("\nEmpleado más proximo a jubilarse: "+aux1.Nombre+" "+aux1.Apellido);
+Console.WriteLine("Fecha de nacimiento: "+aux1.FechaNacimiento.ToString("dd/MM/yyyy"));
+Console.WriteLine("Estado civil: "+aux1.EstadoCivil);
+Console.WriteLine("Género: "+aux1.Genero);
+Console.WriteLine("Fecha de ingreso: "+aux1.FechaIngreso.ToString("dd/MM/yyyy"));
+Console.WriteLine("Sueldo básico: ", aux1.Sueldo);
+Console.WriteLine("Cargo: ", aux1.Cargo);
+
+Console.WriteLine("\nAntigüedad: ", aux1.antiguedad());
+Console.WriteLine("Edad: ", aux1.edad());
+Console.WriteLine("Años para jubilarse: ", aux1.aniosParaJubilarse());
+Console.WriteLine("Salario: ", aux1.salario());
+
